67dce00a4441077458747ad6fed1e3184d70deaf
waelalq <wael.abuomr@gmail.com>
2022-10-10 23:25:40 +0300

    user api login and rigester

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -4,23 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Validator;
 
 class UserAuthController extends Controller
-{
+{    
+
+    use AuthenticatesUsers;
 
     public function register(Request $request)
     {
 
-        $data = $request->validate([
-           
-            'name' => 'required|max:255',
-    
-            'email' => 'required|email|unique:users',
-          
-        ]);
+        $validator = Validator::make($request->all(),$this->validation('rig'),$this->masseges('rig'));
+ 
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+        $data = $request->validate($this->validation('rig'));
         $request['isactive']=true;
 
          $data['password'] = bcrypt($request->password);
@@ -32,16 +37,25 @@ class UserAuthController extends Controller
         return response([ 'user' => $user, 'token' => $token]);
     }
 
+
+    
+
     public function login(Request $request)
     {
-        $data = $request->validate([
-            'email' => 'email|required',
-            'password' => 'required'
-        ]);
+
+        $validator = Validator::make($request->all(),$this->validation('login'),$this->masseges('login'));
+ 
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+
+         $data = $request->validate($this->validation('login'));
+       
 
         if (!auth()->attempt($data)) {
-            return response(['error_message' => 'Incorrect Details. 
-            Please try again']);
+            return response(['error_message' => 'Incorrect Details ! Please try again']);
         }
 
         $token = auth()->user()->createToken('Laravel-9-Passport-Auth')->accessToken;
@@ -49,5 +63,46 @@ class UserAuthController extends Controller
 
         return response(['user' => auth()->user(), 'token' => $token]);
 
+    }
+
+
+
+    protected function validation($used){
+        if($used=='login')
+        return[
+            'phone' => 'required',
+            'password' => 'required'
+        ];
+        else
+        return[
+            'phone' => 'required|unique:users',
+            'password' => 'required',
+            'name' => 'required|max:255',
+        ]; 
+    }
+
+
+
+    protected function masseges($used){
+        if($used=='login')
+        return[
+                'phone.required' => 'We need to know your phone!',
+                'password.required' => 'We need to know your password!',
+          
+        ];
+        else
+        return[
+            'phone.unique:users' => 'alredy used!',
+            'phone.required' => 'We need to know your phone!',
+            'password.required' => 'We need to know your password!',
+            'name.required' => 'We need to know your name!',
+            'name.max:255' => 'must 255!',
+        ]; 
+    }
+
+
+    public function username()
+    {
+        return 'phone';
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('password');
-
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('isactive')->nullable();
